120c58eadd019f496708f76f5f4669619e6903c0
Birma <birma@seznam.cz>
2023-09-13 14:22:50 +0200

    učím se pracovat s databází json

---
+++ src/components/jedenTrik.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="trik" :class="{ complete: trik.complete }">
+        <div class="actions">
+            <h3 @click="showPopis=!showPopis">{{ trik.povel }}</h3>
+            <div class="icons">
+                <q-icon class="ikony" name="edit" size="2em" />
+                <q-icon @click="deleteTrik" class="ikony" name="delete_forever" size="2em" />
+                <q-icon @click="toggleComplete" class="ikony tick" name="check" size="2em" />
+
+            </div>
+        </div>
+        <div class="popis" v-if="showPopis">
+            <p>{{ trik.popis }}</p>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+    props: [ 'trik' ],
+    //druhý atribut se používá pro emitování
+    setup(props, emit) {
+        const showPopis = ref(false)
+        //jen zjednodušení na použití do funkcí        
+        const uri = ref('http://localhost:3000/triky/' + props.trik.id)
+
+        
+        const deleteTrik = () => {
+            fetch (uri.value, { method: 'DELETE' })
+                .then (() => emit.emit('delete', props.trik.id))
+                .catch (err => console.log(err))
+        }
+
+        const toggleComplete = () => {
+            fetch (uri.value, { 
+                method: 'PATCH',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify ({complete: !props.trik.complete})
+            })
+                .then (() => emit.emit('complete', props.trik.id))
+                .catch (err => console.log(err))
+
+        }
+
+
+        return {
+            showPopis,
+            uri,
+            deleteTrik,
+            toggleComplete
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div >
+    <div v-if="triky.length">
+      <div v-for="trik in triky" :key="trik.id">
+        <jedenTrik 
+          :trik="trik"
+          @delete = "handleDelete" 
+          @complete = "handleComplete" 
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import jedenTrik from '@/components/jedenTrik.vue'
+import { onMounted, ref } from 'vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    jedenTrik
+  },
+
+  setup(){
+    
+    const triky = ref([])
+    onMounted (() => {
+      fetch ('http://localhost:3000/triky')
+        .then (res => res.json())
+        .then (data => triky.value = data)
+        .catch (err => console.log(err.message))
+    })
+
+    //předávám sem id mazanýho triku a tady filtruju všechny id triků v poli a vracím (tedy nechávám) jen ty, které neodpovídají id smazaného
+    const handleDelete = (id) => {
+      triky.value = triky.value.filter((trik) => {
+        return trik.id !== id
+      })
+    }    
+    
+    const handleComplete = (id) => {
+      let p = triky.value.find(trik => {
+        return trik.id === id
+      })
+      p.complete = !p.complete
+    }
+    
+    
+    return {
+      triky,
+      handleDelete,
+      handleComplete
+    }
+
   }
 }
 </script>
